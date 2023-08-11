2c25cb4eb86eaac535a3753563042cf013a7d96d
Tunrayo01 <tunrayo93@gmail.com>
2023-08-11 07:07:32 -0700

    put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#include(stdio.h)
+#include<stdio.h>
 
 /**
- * main function put
+ * main -  function put
  *
- * returns 0
+ * Return: always 0(success)
  */
 
 int main(void)
 {
-	puts("\n""Programming is like building a multilingual puzzle");
+	puts("\n"Programming is like building a multilingual puzzle");
 	return(0);
 }
