a53ae6e750bfb563cec9da2592c1bb79514cfc37
Tunrayo01 <tunrayo93@gmail.com>
2023-08-11 16:43:12 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet function
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 'a'; k <= 'z'; k++)
+		putchar(k);
+	putchar('\n');
+}
