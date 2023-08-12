51d251392a9cc38ed97d09f98c6007c63f533513
Tunrayo01 <tunrayo93@gmail.com>
2023-08-11 18:13:53 -0700

    print single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print a based 10 number
+ *
+ * Return: always 0(success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
