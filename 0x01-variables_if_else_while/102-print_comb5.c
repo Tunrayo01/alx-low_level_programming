d58c9d86d9854fe17425095b1848d7944ca6d7f3
Tunrayo01 <tunrayo93@gmail.com>
2023-08-13 13:58:27 -0700

    two digits combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - prints combination of two numbers
@@ -11,13 +13,13 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 0; a < 98; a++)
+	for (a = 0; a <= 98; a++)
 	{
 	for (b = a + 1; b <= 99; b++)
 	{
 	putchar((a / 10) + '0');
 	putchar((a % 10) + '0');
-	putchar(',');
+	putchar(' ');
 	putchar((b / 10) + '0');
 	putchar((b % 10) + '0');
 	if (a == 98 && b == 99)
