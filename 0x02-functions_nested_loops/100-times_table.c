30cb8f9391c126d33db91d6b6e3f3088d2796b3a
Tunrayo01 <tunrayo93@gmail.com>
2023-08-17 00:56:14 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,10 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * print_times_table - print n times table from 0
  * @n: number to be used
  */
+
 void print_times_table(int n)
 {
 	int a, b, c;
@@ -11,37 +13,26 @@ void print_times_table(int n)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b <= n; b++)
-			{
-			c = b * a;
-			if (b == 0)
-			{
-			_putchar(c + '0');
-			}
-			else if (c < 10 && b != 0)
+			_putchar('0');
+			for (b = 1; b <= n; b++)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			}
-			else if (c >= 10 && c < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-			else if (c >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
+				c = a * b;
+				if (c <= 99)
+					_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
+				if (c >= 100)
+				{
 				_putchar((c / 100) + '0');
 				_putchar(((c / 10) % 10) + '0');
+				}
+				else if (c <= 99 && c >= 10)
+				{
+				_putchar((c / 10) + '0');
+				}
 				_putchar((c % 10) + '0');
-			}
 			}
 			_putchar('\n');
 		}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ * numLenght - lenght of string
+ * @num: operand number
+ * Return: lengt of digits
+ */
+
+int numLength(int num)
+{
+	int lenght = 0;
+
+	if (!num)
+	{
+	return (1);
+	}
+	while (num)
+	{
+		num = num / 10;
+		lenght = lenght + 1;
+	}
+	return (lenght);
+}
+
+/**
+ * main - calculat the firste 98 fibonnaci numbers
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f1o = 0, f2o = 0, tmpo = 0;
+	short int x = 1, initial0s;
+
+	while (x <= 98)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLenght(f1);
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%i", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
+		tmp = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = tmp;
+		f2o = tmpo;
+
+		if (x != 98)
+			printf(",");
+		else
+			printf("\n");
+		x++;
+	}
+	return (0);
+}
