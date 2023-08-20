e474cae626be6d32376a29dc21aba1ea1f0c6f9b
Tunrayo01 <tunrayo93@gmail.com>
2023-08-20 11:46:53 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,12 @@ void more_numbers(void)
 	int a;
 	int b;
 
-	for (a = 0; a <= 9; ++a)
+	for (a = 1; a <= 10; a++)
 	{
-		for (b = 0; b <= 14; ++b)
+		for (b = 0; b <= 14; b++)
 		{
-			if (b < 9)
-			_putchar((b / 10) + 48);
+			if (b >= 10)
+			_putchar('1');
 			else
 			_putchar((b % 10) + 48);
 		}
