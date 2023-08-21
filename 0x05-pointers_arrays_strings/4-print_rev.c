2fd8c16d6a60f2d918418f9d992b8d3461945965
Tunrayo01 <tunrayo93@gmail.com>
2023-08-21 15:36:22 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,8 +12,12 @@ void print_rev(char *s)
 	int counter = 0;
 
 	for (j = 0; s[j] != '\0'; j++)
+	{
 		counter++;
-		for (j = counter - 1; j >= 0; j++)
-			_putchar(s[j]);
+	}
+	for (j = counter - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
 	_putchar('\n');
 }
