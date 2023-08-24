d0c21f11335c26569e9606dc922e0242576613ce
Tunrayo01 <tunrayo93@gmail.com>
2023-08-23 21:04:47 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,18 @@ void print_square(int size)
 	int a, b;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
-	for (a = 0; a < size; a++)
-	{
-	for (b = 0; b > size; b++)
-	{
-		_putchar('#');
 	}
+	else
+	{
+		for (b = 0; b < size; b++)
+		{
+		for (a = 0; a < size; a++)
+		{
+		_putchar('#');
+		}
 	_putchar('\n');
+		}
 	}
 }
