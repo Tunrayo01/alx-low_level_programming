e82c849e47969bd73547332bb7378fbd6b734096
Tunrayo01 <tunrayo93@gmail.com>
2023-09-11 14:42:06 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_MACRO
+#define DOG_MACRO
+
+/**
+ * struct dog - structure dog
+ * @name: name pararmeter
+ * @age: age pararmeter
+ * @owner: owner pararmeter
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
