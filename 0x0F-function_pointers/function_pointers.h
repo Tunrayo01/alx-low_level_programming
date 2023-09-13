e7ce0338b4e7a34d693565cb4ac191d93d60c2ea
Tunrayo01 <tunrayo93@gmail.com>
2023-09-13 15:37:22 -0700

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
