7c3083d2be83a5cbcb3f47bc9ac710cda163e3bc
Tunrayo01 <tunrayo93@gmail.com>
2023-09-13 17:19:42 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		printf("Error/n");
+		printf("Error\n");
 		return (1);
 	}
 	int num_bytes = atoi(argv[1]);
 
 	if (num_bytes < 0)
 	{
-		printf("Error/n");
+		printf("Error\n");
 		return (2);
 	}
 
