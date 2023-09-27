bc521870ee292e61f3fd540bcc1c9794ab66acc1
Tunrayo01 <tunrayo93@gmail.com>
2023-09-27 14:29:59 -0700

    print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,12 +16,10 @@ size_t print_listint_safe(const listint_t *head)
 	temp = head;
 	if (temp == 0)
 		return (0);
-
 	while (temp != 0)
 	{
 		temp2 = temp;
 		temp = temp->next;
-
 		num++;
 
 		printf("[%p] %d\n", (void *)temp2, temp2->n);
