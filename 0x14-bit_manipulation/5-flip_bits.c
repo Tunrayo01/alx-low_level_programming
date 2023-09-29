a108cf1e418a1e8f1e34af0e0c6f71179641d957
Tunrayo01 <tunrayo93@gmail.com>
2023-09-28 17:04:25 -0700

    result

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,5 +8,13 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	
+	unsigned long int result = n ^ m;
+	unsigned int count = 0;
+
+	while (result > 0)
+	{
+	count += result & 1;
+	result >>= 1;
+	}
+	return (count);
 }
